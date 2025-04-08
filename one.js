ed49b680406c8d5629b774d16ba8a9f72f694c4a
Priyanka Mehta <primehta17@gmail.com>
2025-04-08 13:02:43 +0530

    celsius to fehrenheit

---
+++ one.js
@@ -0,0 +1,17 @@
+// Celsius to Fahrenheit
+
+function fahrenheit(cels){
+    return f=(cels * 1.8) + 32;
+}
+
+console.log(fahrenheit(55));
+
+// Enter a celsius value: 55
+// 55 degree celsius is equal to 131 degree fahrenheit.
+
+
+function celsius(fahren){
+    return (fahren - 32)/1.8;
+}
+
+console.log(celsius(131))
