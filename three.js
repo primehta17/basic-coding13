750b465eb9c7c2df9a55f83fe9f665a9f3651f7f
Priyanka Mehta <primehta17@gmail.com>
2025-04-08 13:20:23 +0530

    even or odd

---
+++ three.js
@@ -0,0 +1,26 @@
+//Check if a number is Positive, Negative, or Zero
+
+function interger(nums){
+    if(nums>0){
+        console.log("number is positive");
+    }else if(nums==0){
+        console.log("number is equal to zero");
+    }else{
+        console.log("number is negative");
+    }
+}
+
+interger(-1.2);
+interger(0);
+interger(1.2);
+
+function evenorodd(num){
+    if(num%2==0){
+        console.log("number is even");
+    }else{
+        console.log("number is odd");
+    }
+}
+evenorodd(23);
+evenorodd(32);
+
